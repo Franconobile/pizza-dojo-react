e8dc84e08a1ba804c406d417445ca793989ebb8d
Franconobile <eifrann22@gmail.com>
2022-11-08 22:51:54 -0300

    Ya casi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavWrapper } from "./Navbarstyle";
-import { Link } from "react-router-dom";
+import { BgDiv } from "./Navbarstyle";
+import { FaUserAlt } from "react-icons/fa"; 
+import { BsCartFill } from "react-icons/bs";
+import BurgerBtn from "./BurgerBtn";
 
 const Navbar = () => {
+
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    // Aprendi que hacer esto sirve para cuando esta true lo pase a false y vice versa
+    setClicked(!clicked)
+  }
+
+  
     return (
         <>
          <NavWrapper>
           <div className="navbar_logo">
             <img src={require('../assets/Pizzalogo.png')} id="logo" alt="Logo"/>
           </div>
+          <div className="navbar_elements">
+              <div className={`links ${clicked ? 'active' : ''}`}>
+              <a onclick={handleClick} href="#h"><FaUserAlt style={{marginRight:'.5rem'}}/>Accede</a>
+              <a onclick={handleClick} href="#h">Productos</a>
+              <a onclick={handleClick} href="#h">Contactanos</a>
+              <a onclick={handleClick} href="#H"><BsCartFill style={{fontSize: "1.5em"}}/></a>
+            </div>
+          </div>
+          <div className="burger">
+          <BurgerBtn clicked={clicked} handleClick={handleClick}/>
+          </div>
          </NavWrapper>
-        
+         <BgDiv className={`initial ${clicked ? 'active' : ''}`}></BgDiv>
 
 
         </>
--- src/components/Navbar/Navbarstyle.js
+++ src/components/Navbar/Navbarstyle.js
@@ -8,68 +8,101 @@ export const NavWrapper = styled.nav`
     align-items: center;
     width: 100%;
     height: 65px;
-    padding: 0px 30px;
     position: fixed;
     top: 0;
 
-    
-    h2 {
-        font-weight: 400;
-        color: #fff;
-        font-size: 1rem;
-        @media (min-width: 768px) {
-          font-size: 1.5rem;
-        }
-      }
-
       #logo {
-        width: 50px;
+        width: 70px;
         padding-left: 20px;
+        margin-left: 10px;
       }
 
       #logo:hover {
         transform: rotate(90deg);
         transition: 1s ease;
       }
-
+      
       a {
-        color: #fff;
+        color: var(--text-color-2);
         text-decoration: none;
         margin-right: 1rem;
+        padding-right: 1.5rem;
+        font-weight: 800;
+        font-size: 16px;
+        word-spacing: 2px;
       }
-      .burger {
-        @media (min-width: 768px) {
-          display: none;
-        }
+    
+      .navbar_elements a:hover {
+        color: #CA8C35;
+    }
+
+    .burger {
+      @media(min-width: 768px){
+        display: none;
       }
-      .links {
-        position: absolute;
-        top: -700px;
-        left: -2000px;
-        right: 0;
-        margin: auto;
-        text-align: center;
-        transition: all 0.6s ease;
-        a {
-          color: #fff;
-          font-size: 2rem;
-          display: block;
-          transition: all .3s;
-          &:hover{
-            text-shadow: 0 0 0.8em #02b3ab;
-        }
-        }
-        
-        @media (min-width: 768px) {
-          position: initial;
-          margin: 0;
-          a {
-            font-size: 1rem;
-            color: white;
-            display: inline;
-          }
-          display: block;
-        }
+    }
+
+    .links {
+      position: absolute;
+      top: -700px;
+      right: -2000px;
+      right: 0;
+      margin-left: auto;
+      margin-right: auto;
+      text-align: center;
+      transition: all .5s ease;
+      a {
+        color: var(--text-color-2);
+        font-size: 2rem;
+        display: block;
       }
+      @media(min-width: 768px){
+        position: initial;
+        margin: 0;
+        a{
+          font-size: 1rem;
+          color: var(--text-color-2);
+          display: inline;
+        }
+        display: block;
+      }
+    }
+
+    .links.active {
+      width: 100%;
+      display: block;
+      position: absolute;
+      margin-left: auto;
+      margin-right: auto;
+      top: 200%;
+      left: 20%;
+      right: 0;
+      text-align: center;
+
+      a {
+        margin-top: 2rem;
+        color: var(--text-color-2);
+      }
+    }
+`
+
+export const BgDiv = styled.div`
+    position: absolute;
+    background-color: #121619;
+    top: -700px;
+    left: -1000px;
+    width: 100%;
+    height: 100%;
+    
+
+
+    &.active {
+      border-radius: 0 0 0 80%;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+    }
 
 `
